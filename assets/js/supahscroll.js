e1785d718ae35634346530cb2b664e58d691b09f
imjihyeon00 <imjihyeon00@gmail.com>
2021-11-22 13:16:40 +0900

    add index4

---
+++ assets/js/supahscroll.js
@@ -0,0 +1,79 @@
+// ## Instancing
+// const supahscroll = new SupahScroll({
+//   el: 'main', // your element or classname or ID
+//   speed: 0.1 // lower is slower, this value could go from 0.01 to 1
+// })
+
+// ## Methods
+// - .update() // update and resize
+// - .pause() // pause, every scroll is stopped
+// - .play() // play, back to normality after pause
+// - .scrollTo(y) // go to your offsetY smoothly
+// - .staticScrollTo(y) // go to your offsetY without animation
+// - .destroy() // kaput, au revoir, kthxbye
+
+/*------------------------------
+SupahScroll
+------------------------------*/
+class SupahScroll {
+    constructor(options) {
+        this.opt = options || {};
+        this.el = this.opt.el ? this.opt.el : '.supah-scroll';
+        this.speed = this.opt.speed ? this.opt.speed : 0.1;
+        this.init();
+    }
+    init() {
+        this.scrollX = 0;
+        this.supahScroll = document.getElementById(this.el);
+        this.supahScroll.classList.add('supah-scroll');
+        this.events();
+        this.update();
+        this.animate();
+    }
+    update() {
+        if (this.supahScroll === null) return;
+        document.body.style.height = `${this.supahScroll.getBoundingClientRect().width}px`;
+    }
+    pause() {
+        document.body.style.overflow = 'hidden';
+        cancelAnimationFrame(this.raf);
+    }
+    play() {
+        document.body.style.overflow = 'inherit';
+        this.raf = requestAnimationFrame(this.animate.bind(this));
+    }
+    destroy() {
+        this.supahScroll.classList.remove('supah-scroll');
+        this.supahScroll.style.transform = 'none';
+        document.body.style.overflow = 'inherit';
+        window.removeEventListener('resize', this.update);
+        cancelAnimationFrame(this.raf);
+        delete this.supahScroll;
+    }
+    animate() {
+        this.scrollX += (window.scrollY - this.scrollX) * this.speed;
+        this.supahScroll.style.transform = `translate3d(${-this.scrollX}px,0,0)`;
+        this.raf = requestAnimationFrame(this.animate.bind(this));
+    }
+    scrollTo(x) {
+        window.scrollTo(x, 0);
+    }
+    staticScrollTo(x) {
+        cancelAnimationFrame(this.raf);
+        this.scrollX = x;
+        window.scrollTo(x, 0);
+        this.supahScroll.style.transform = `translate3d(${-x}px,0,0)`;
+        this.play();
+    }
+    events() {
+        window.addEventListener('load', this.update.bind(this));
+        window.addEventListener('resize', this.update.bind(this));
+    }
+}
+/*------------------------------
+Initialize
+------------------------------*/
+const supahscroll = new SupahScroll({
+    el: 'horizon',
+    speed: 0.05
+});
